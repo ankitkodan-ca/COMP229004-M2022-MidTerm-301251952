7278945566be561235ff90a425e6487087aee9eb
ankitkodan-ca <ankitkodan.ca@gmail.com>
2022-06-25 14:02:36 -0400

    final

---
+++ config/db.js
@@ -0,0 +1,18 @@
+// Do not expose your credentials in your code.
+let atlasDB = "mongodb+srv://test:ulfH4o2c30i7OIbU@cluster0.c0dbj.mongodb.net/?retryWrites=true&w=majority";
+
+// Database setup
+let mongoose = require('mongoose');
+
+module.exports = function(){
+
+    mongoose.connect(atlasDB);
+    let mongodb = mongoose.connection;
+
+    mongodb.on('error', console.error.bind(console, 'Connection Error:'));
+    mongodb.once('open', ()=>{
+        console.log('===> Connected to MongoDB.');
+    })
+
+    return mongodb;
+}
---
+++ controllers/car.js
@@ -0,0 +1,168 @@
+// create a reference to the model
+const { deleteModel } = require('mongoose');
+const { deleteOne } = require('../models/car');
+const car = require('../models/car');
+let CarModel = require('../models/car');
+
+// Gets all cars from the Database and renders the page to list them all.
+module.exports.carList = function(req, res, next) {  
+    CarModel.find((err, carsList) => {
+        //console.log(carList);
+        if(err)
+        {
+            return console.error(err);
+        }
+        else
+        {
+            res.render('cars/list', {
+                title: 'Cars List', 
+                CarsList: carsList,
+                userName: req.user ? req.user.username : ''
+            })            
+        }
+    });
+}
+
+
+// Gets a car by id and renders the details page.
+module.exports.details = (req, res, next) => {
+    
+    let id = req.params.id;
+
+    CarModel.findById(id, (err, carToShow) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //show the edit view
+            res.render('cars/details', {
+                title: 'Car Details', 
+                car: carToShow
+            })
+        }
+    });
+}
+
+// Renders the Add form using the add_edit.ejs template
+module.exports.displayAddPage = (req, res, next) => {
+    
+    // ADD YOUR CODE HERE   
+    let newCar = CarModel();
+
+    res.render('cars/add_edit', {
+        title: 'Add a new Car',
+        car: newCar,
+        userName: req.user ? req.user.username : ''
+    })
+}
+
+// Processes the data submitted from the Add form to create a new car
+module.exports.processAddPage = (req, res, next) => {
+
+    // ADD YOUR CODE HERE
+    let newCar = CarModel({
+        _id: req.body.id,
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        kilometers: req.body.kilometers,
+        doors: req.body.doors,
+        seats: req.body.seats,
+        color: req.body.color,
+        price: req.body.price
+    });
+
+    CarModel.create(newCar, (err, car) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            console.log(car);
+            res.redirect('/cars/list');
+        }
+    });
+
+}
+
+// Gets a car by id and renders the Edit form using the add_edit.ejs template
+module.exports.displayEditPage = (req, res, next) => {
+    
+    // ADD YOUR CODE HERE
+    let id = req.params.id;
+
+    CarModel.findById(id, (err, carToShow) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            res.render('cars/add_edit', {
+                title: 'Edit Model',
+                car: carToShow,
+                userName: req.user ? req.user.username : ''
+            })
+        }
+    });
+}
+
+// Processes the data submitted from the Edit form to update a car
+module.exports.processEditPage = (req, res, next) => {
+    
+    // ADD YOUR CODE HERE
+    let id = req.params.id;
+
+    let updatedCar = CarModel({
+        _id: req.body.id,
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        kilometers: req.body.kilometers,
+        doors: req.body.doors,
+        seats: req.body.door,
+        color: req.body.color,
+        price: req.body.price,
+    });
+
+    CarModel.updateOne({_id: id}, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            res.redirect('/cars/list');
+        }
+    });
+  
+}
+
+// Deletes a car based on its id.
+
+module.exports.performDelete = (req, res, next) => {
+
+    let id = req.params.id;
+
+
+    CarModel.remove({_id: id}, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // refresh the cars list
+            res.redirect('/cars/list');
+        }
+    });
+
+}
